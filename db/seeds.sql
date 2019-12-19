2865c331eef85cfc9dd0518555af81d91847abf2
DwayneL <dr2@ameritech.net>
2019-12-18 21:19:39 -0600

    initial commit. set up. organized folder paths and started mysql queries

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured);
+VALUES (cheeseburger, true);
+
+INSERT INTO burgers (burger_name, devoured);
+VALUES (bacon burger, false);
+
+INSERT INTO burgers (burger_name, devoured);
+VALUES (hamburger, true);
