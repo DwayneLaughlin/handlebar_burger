7a3ffc888bcacde326fbc21b24ec75b2903cc5d6
DwayneL <dr2@ameritech.net>
2019-12-22 18:21:50 -0600

    updates to orm

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id: INT NOT NULL AUTO_INCREMENT;
-    buger_name: VARCHAR (100);
-    devoured: BOOLEAN;
-    PRIMARY KEY (id);
+    id INT NOT NULL AUTO_INCREMENT,
+    buger_name VARCHAR (100),
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
 );
